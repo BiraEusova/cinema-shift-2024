0611a6ad46f08ed67162a1c580da90e59c8b2ece
irabr <breusova@tpu.ru>
2024-07-08 19:53:24 +0700

    Минорные стилевые изменения

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import HeaderButton from "./ui/HeaderButton.tsx";
 import FilmCard from "./components/FilmCard.tsx";
 const App = () => {
 
+    //TODO: что такое переменные CSS
+
     return (
         <>
             <header className='header'>
@@ -25,7 +27,8 @@ const App = () => {
             </header>
 
             <div style={{
-                marginTop: "60px",
+                marginTop: "48px",
+                paddingBottom: "48px",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center"
@@ -33,12 +36,22 @@ const App = () => {
 
                 <div style={{
                     width: '960px',
+                    gap: '16px'
                 }}>
-                    <h3 style={{width: '100%', textAlign: 'left'}}>Афиша</h3>
+                    <h3 style={{
+                        width: '100%',
+                        textAlign: 'left',
+                        fontFamily: "Inter, sans-serif",
+                        fontSize: "24px",
+                        fontWeight: 700,
+                        padding: "0 16px",
+                        margin: 0
+                    }}>Афиша</h3>
                     <div style={{
                         display: 'grid',
                         gridTemplateColumns: "repeat(3,1fr)",
-                        gap: '60px 20px'
+                        gap: '48px 20px',
+                        margin: "16px 0 0 0"
                     }}>
                         <FilmCard />
                         <FilmCard />
